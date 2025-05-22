09855fbecd5df27c20cdcb6499fdedeb78e2de11
House of pwn <211771648+houseofpwn@users.noreply.github.com>
2025-05-21 22:20:56 -0600

    Cleanup

--- depvulnanalyzer.py
+++ depvulnanalyzer.py
@@ -1,33 +1,44 @@
 import json
 import sys
 
-from colorama import Fore, Back, Style
+from colorama import Fore
 import re
 
 def loaddeps(depfile):
     print(Fore.YELLOW + "Loading dependencies...")
-    with open(depfile, "r") as f:
-        deps = json.load(f)
-        f.close()
+    try:
+        with open(depfile, "r") as f:
+            deps = json.load(f)
+            f.close()
+    except FileNotFoundError:
+        print(f'File {depfile} not found')
+        exit(1)
     return deps
 
 def loadnotfixed(notfixedfile):
     print(Fore.CYAN + "Loading non-fixed dependencies...")
-    with open(notfixedfile, "r") as f:
-        notfixed = json.load(f)
-        f.close()
+    try:
+        with open(notfixedfile, "r") as f:
+            notfixed = json.load(f)
+            f.close()
+    except FileNotFoundError:
+        print(f'File {notfixedfile} not found')
+        exit(1)
     return notfixed
 
 def loadfixed(fixedfile):
     print(Fore.YELLOW + "Loading fixed dependencies...")
-    with open(fixedfile, "r") as f:
-        fixed = json.load(f)
-        f.close()
+    try:
+        with open(fixedfile, "r") as f:
+            fixed = json.load(f)
+            f.close()
+    except FileNotFoundError:
+        print(f'File {fixedfile} not found')
+        exit(1)
     return fixed
 
-def isdepvulnerable(name, vuln, deps, notfixedstatus):
+def isdepvulnerable(name, deps, notfixedstatus):
     for dep in deps:
-        #print(f'Checking for {name} in {dep}')
         deplist = deps[dep]
         if name in deplist:
             status = {
@@ -41,11 +52,9 @@ def isdepvulnerable(name, vuln, deps, notfixedstatus):
     return notfixedstatus
 
 def isbetweenversions(low, high, current):
-    #print(Fore.WHITE + f'Checking if {low} < {current} < {high}')
     lowlist = re.split(r'[.-]', low)
     highlist = re.split(r'[.-]', high)
     currentlist = re.split(r'[.-]', current)
-    failed = False
     index = 0
     minmet = True
     for v in currentlist:
@@ -53,7 +62,7 @@ def isbetweenversions(low, high, current):
             lowtemp = '0'
         else:
             lowtemp = lowlist[index]
-        if ( int(v) < int(lowtemp)):
+        if int(v) < int(lowtemp):
             minmet = False
             break
         index += 1
@@ -81,6 +90,7 @@ def isbetweenversions(low, high, current):
     return failed
 
 def checkdepfixed(name, vuln, deps, fixedstatus, docsdir):
+    # This is a sentinel for the upcoming loop for formatting.
     lastname = "xx"
     for dep in deps:
         deplist = deps[dep]
@@ -119,7 +129,7 @@ def checkunfixedvulns(deps, notfixed, docsdir):
     for cve, libobjs in notfixed.items():
         for item in libobjs:
             if type(libobjs[item]) is dict:
-                notfixedstatus = isdepvulnerable(item, libobjs[item], deps, notfixedstatus)
+                notfixedstatus = isdepvulnerable(item, deps, notfixedstatus)
     with open(f'{docsdir}/fixed-status.json', "w+") as f:
         json.dump(notfixedstatus, f, indent=4)
         f.close()
